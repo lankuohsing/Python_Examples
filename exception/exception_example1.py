9b7f336e21fc466bb1c7de8752381e196b91d036
lankuohsing <lankuohsing@163.com>
2022-03-22 16:35:32 +0800

    add exception_examples

---
+++ exception/exception_example1.py
@@ -0,0 +1,50 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar 22 16:13:02 2022
+
+@author: lankuohsing
+"""
+
+person_info={"Name":"Jack","Age":20,"Gender":"Male"}
+try:
+    print(person_info["Weight"])
+except KeyError:
+    print("Key Weight doesn't exist!")
+print(person_info["Age"])
+print(person_info["Gender"])
+
+
+
+# In[]
+person_info={"Name":"Jack","Age":20,"Gender":"Male","Weight":10,"Height":0}
+try:
+    print(person_info["Weight"])
+    print(person_info["Height"])
+    BMI=person_info["Weight"]/(person_info["Height"])
+except KeyError:
+    print("Key Weight doesn't exist!")
+except ZeroDivisionError:
+    print("Divided by Height but Height is zero!")
+print(person_info["Age"])
+print(person_info["Gender"])
+
+
+# In[]
+s = "123"
+try:
+    if s is None or s=="123":#试试把s=="123"去掉
+        print("s 是空对象 or s==123")
+        # 未声明/初始化对象 (没有属性)
+        raise NameError     #如果引发NameError异常，后面的代码将不能执行
+    print(len(s))
+except NameError:
+    print("空对象没有长度")
+else:
+    print(len(s),"again")
+print(111)
+
+"""
+s 是空对象 or s==123
+空对象没有长度
+111
+"""
